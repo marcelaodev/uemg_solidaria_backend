0840db6ee2513bdd35081485074c416340f05868
Marcelo B Côrtes <marcelobcortes@gmail.com>
2019-10-02 20:44:22 -0300

    esboço das rotas

--- config/routes/privateRoutes.js
+++ config/routes/privateRoutes.js
@@ -1,5 +1,13 @@
 const privateRoutes = {
   'GET /users': 'UserController.getAll',
+  'GET /user/{id}': 'UserController.get',
+  'PUT /user/{id}': 'UserController.changeGrupo',
+
+
+  // CRUD campanha
+
+  // CRUD doacao
+
 };
 
 module.exports = privateRoutes;
--- config/routes/publicRoutes.js
+++ config/routes/publicRoutes.js
@@ -3,6 +3,16 @@ const publicRoutes = {
   'POST /register': 'UserController.register', // alias for POST /user
   'POST /login': 'UserController.login',
   'POST /validate': 'UserController.validate',
+
+  'GET /campanha': 'CampanhaController.getAll',
+  'GET /campanha/{id}': 'CampanhaController.get',
+  'GET /campanha/{id}/grupo': 'CampanhaController.getRankingGrupo',
+  'GET /campanha/{id}/individual': 'CampanhaController.getRankingIndividual',
+
+  'GET /grupo': 'GrupoController.getAll',
+  'GET /grupo/{id}/alfabetico': 'GrupoController.getIntegrantesAlfabetico',
+  'GET /grupo/{id}/cronologico': 'GrupoController.getIntegrantesCronologico',
+  'GET /grupo/{id}/doacao/{campanha}': 'GrupoController.getIntegrantesDoacao',
 };
 
 module.exports = publicRoutes;
